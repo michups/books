823e7e85ba158334746eb5aab045dd14bf47eb41
michal <michalwir@gmail.com>
2017-06-01 17:26:04 +0200

    Create basic book class

---
+++ src/main/java/com/michups/book/Book.java
@@ -0,0 +1,37 @@
+package com.michups.book;
+
+/**
+ * Created by michups on 01.06.17.
+ */
+public class Book {
+    private String title;
+    private String author;
+    private Integer yearOfPublish;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getYearOfPublish() {
+        return yearOfPublish;
+    }
+
+    public void setYearOfPublish(Integer yearOfPublish) {
+        this.yearOfPublish = yearOfPublish;
+    }
+
+
+}
---
+++ src/main/java/com/michups/book/MainBook.java
@@ -0,0 +1,10 @@
+package com.michups.book;
+
+/**
+ * Created by michups on 01.06.17.
+ */
+public class MainBook {
+    public static void main(String[] args) {
+
+    }
+}
